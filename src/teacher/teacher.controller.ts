8c3aa3c23fefb9844a21b6deb80ac7fcc7fabce7
JRKKRUPICKI <jrkkrupicki@gmail.com>
2022-10-28 13:42:16 +0200

    Dodanie rejestracji kont nauczycieli

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -6,9 +6,9 @@ import { LoginTeacherDto } from "./dto/login-teacher.dto";
 import { TeacherService } from "./teacher.service";
 
 @Controller('teacher')
-export class TeacherController{
-    constructor(private teacherService: TeacherService){}
-    
+export class TeacherController {
+    constructor(private teacherService: TeacherService) { }
+
     /*
     SPOSÓB 1
     private userService;
@@ -22,37 +22,43 @@ export class TeacherController{
 
     // GET /teacher
     @Get()
-    getTeachers(){
+    getTeachers() {
         return this.teacherService.getAll();
     }
 
     // GET /teacher/1
     @Get(':id')
-    getTeacher(@Param() params: GetTeacherDto){
+    getTeacher(@Param() params: GetTeacherDto) {
         return this.teacherService.getById(params.id);
     }
 
     // POST /teacher
     @Post()
-    addTeacher(@Body() body: AddTeacherDto){
+    addTeacher(@Body() body: AddTeacherDto) {
         return this.teacherService.add(body.email, body.password, body.password2);
     }
 
     // DELETE /teacher/1
     @Delete(':id')
-    removeTeacher(@Param() params: GetTeacherDto){
+    removeTeacher(@Param() params: GetTeacherDto) {
         return this.teacherService.remove(params.id);
     }
 
     // PATCH /teacher/1
     @Patch(':id')
-    editTeacher(@Param() params: GetTeacherDto, @Body() body: EditTeacherDto){
+    editTeacher(@Param() params: GetTeacherDto, @Body() body: EditTeacherDto) {
         return this.teacherService.edit(params.id, body.password, body.password2);
     }
 
     // POST /teacher/login
     @Post('login')
-    loginTeacher(@Body() body: LoginTeacherDto){
+    loginTeacher(@Body() body: LoginTeacherDto) {
         return this.teacherService.login(body.email, body.password);
+    }
+
+    // POST /teacher/register
+    @Post('register')
+    registerTeacher(@Body() body: LoginTeacherDto) {
+        return this.teacherService.register(body.email, body.password);
     }
 }
--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,81 +1,93 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { ILike, Repository } from "typeorm";
 import { Teacher } from "./teacher.entity";
 
 @Injectable()
-export class TeacherService{
-    constructor(@InjectRepository(Teacher) private repo: Repository<Teacher>){}
+export class TeacherService {
+    constructor(@InjectRepository(Teacher) private repo: Repository<Teacher>) { }
 
-    getAll(){
+    getAll() {
         return this.repo.find({
             relations: ['tests']
         });
     }
 
-    async getById(id: number){
+    async getById(id: number) {
         let teacher = null;
         await this.repo.findOne({
             where: {
                 id: id
             }
         }).then(t => teacher = t);
-        if(!teacher) throw new HttpException('Teacher not found', HttpStatus.BAD_REQUEST);
+        if (!teacher) throw new HttpException('Teacher not found', HttpStatus.BAD_REQUEST);
         return teacher;
     }
 
-    async add(email: string, password: string, password2: string){
+    async add(email: string, password: string, password2: string) {
         let teacher = null;
         await this.repo.findOne({
             where: {
                 email: email
             }
         }).then(t => teacher = t);
-        if(teacher) throw new HttpException('Teacher already exists', HttpStatus.BAD_REQUEST);
-        if(password !== password2) throw new HttpException('First password is not same as second password', HttpStatus.BAD_REQUEST);
+        if (teacher) throw new HttpException('Teacher already exists', HttpStatus.BAD_REQUEST);
+        if (password !== password2) throw new HttpException('First password is not same as second password', HttpStatus.BAD_REQUEST);
         // TODO hashed password
-        teacher = this.repo.create({email, password});
+        teacher = this.repo.create({ email, password });
         teacher = await this.repo.save(teacher);
         delete teacher.password;
         return teacher;
     }
 
-    async remove(id: number){
+    async remove(id: number) {
         let teacher = null;
         await this.repo.findOne({
             where: {
                 id: id
             }
         }).then(t => teacher = t);
-        if(!teacher) throw new HttpException('Teacher not found', HttpStatus.BAD_REQUEST);
+        if (!teacher) throw new HttpException('Teacher not found', HttpStatus.BAD_REQUEST);
         this.repo.remove(teacher);
     }
 
-    async edit(id: number, password: string, password2: string){
+    async edit(id: number, password: string, password2: string) {
         let teacher = null;
         await this.repo.findOne({
             where: {
                 id: id
             }
         }).then(t => teacher = t);
-        if(!teacher) throw new HttpException('Teacher not found', HttpStatus.BAD_REQUEST);
-        if(password !== password2) throw new HttpException('First password is not same as second password', HttpStatus.BAD_REQUEST);
+        if (!teacher) throw new HttpException('Teacher not found', HttpStatus.BAD_REQUEST);
+        if (password !== password2) throw new HttpException('First password is not same as second password', HttpStatus.BAD_REQUEST);
         teacher.password = password;
         teacher = await this.repo.save(teacher);
         delete teacher.password;
         return teacher;
     }
 
-    async login(email: string, password: string){
+    async login(email: string, password: string) {
         let teacher = null;
         await this.repo.findOne({
             where: {
-                email: email,
+                email: ILike(email),
                 password: password
             }
         }).then(t => teacher = t);
-        if(!teacher) throw new HttpException('Incorrent email or password', HttpStatus.UNAUTHORIZED);
+        if (!teacher) throw new HttpException('Incorrent email or password', HttpStatus.UNAUTHORIZED);
         delete teacher.password;
         return teacher;
+    }
+
+    async register(email: string, password: string) {
+        let teacher = null;
+        await this.repo.findOne({
+            where: {
+                email: ILike(email)
+            }
+        }).then(t => teacher = t);
+        if (teacher) throw new HttpException('Teacher already exists', HttpStatus.BAD_REQUEST);
+        teacher = this.repo.create({ email, password });
+        return this.repo.save(teacher);
     }
 }
